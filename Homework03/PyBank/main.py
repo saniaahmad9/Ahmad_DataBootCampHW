46d616c04b56e474d476701ed1b78ab210146748
Sania Ahmad <saniaahmad@Sanias-MacBook-Pro.local>
2019-06-29 17:26:14 -0400

    Loading homework 3

--- Homework03/PyBank/main.py
+++ Homework03/PyBank/main.py
@@ -1,1 +1,49 @@
+import csv
+
+budget_data = "/Users/saniaahmad/Desktop/Ahmad_DataBootCampHW/Homework03/PyBank/budget_data.csv"
+
+total_months = 0
+prev_revenue = 0
+month_of_change = []
+revenue_change_list = []
+greatest_increase = ["", 0]
+greatest_decrease = ["", 1000000000000000000]
+total_revenue = 0
+
+with open(budget_data, newline="") as csvfile:
+    reader = csv.reader(csvfile, delimiter=",")
+    
+    csv_header = next(reader)
+    print(f"Header: {csv_header}")
+    
+    for row in reader:
+            total_months = total_months + 1
+            total_revenue = total_revenue + int(row[1])
+            
+            revenue_change = int(row[1]) - prev_revenue
+            prev_revenue = int(row[1])
+            revenue_change_list = revenue_change_list + [revenue_change]
+            month_of_change = month_of_change + [row[0]]
+            
+            if (revenue_change > greatest_increase[1]):
+                greatest_increase[0] = row[0]
+                greatest_increase[1] = revenue_change
+                
+            
+            if (revenue_change < greatest_decrease[1]):
+                greatest_decrease[0] = row[0]
+                greatest_decrease[1] = revenue_change
+            
+                
+revenue_avg = sum(revenue_change_list) / len(revenue_change_list)
+
+print(revenue_change)
+
+print("Financial Analysis")
+print("-------------------------")
+print("Total Months: " + str(total_months))
+print("Total Revenue: " + "$" + str(total_revenue))
+print("Greatest Increase: " + str(greatest_increase[0]) + " ($" +  str(greatest_increase[1]) + ")") 
+print("Greatest Decrease: " + str(greatest_decrease[0]) + " ($" +  str(greatest_decrease[1]) + ")")
+    
 
--- Homework03/PyPoll/main.py
+++ Homework03/PyPoll/main.py
@@ -1,1 +1,37 @@
+import csv
 
+poll_data = "/Users/saniaahmad/Desktop/GWARL201906DATA1/02-Homework/03-Python/Instructions/PyPoll/Resources/election_data.csv"
+
+votes = 0
+winner_votes = 0
+candidates = 0
+candidate_choice = []
+candidate_votes = {}
+
+
+with open(poll_data) as poll_data:
+    reader = csv.DictReader(poll_data)
+
+    for row in reader:
+        votes = votes + 1
+        candidates = row["Candidate"]        
+
+        if row["Candidate"] not in candidate_choice:
+            
+            candidate_choice.append(row["Candidate"])
+
+            candidate_votes[row["Candidate"]] = 1
+            
+        else:
+            candidate_votes[row["Candidate"]] = candidate_votes[row["Candidate"]] + 1
+
+print("Election Results")
+print("-------------------------")
+print("Total Votes: " + str(votes))
+print("-------------------------")
+
+for c in candidate_votes:
+        print(c + " " + str(round(((candidate_votes[c]/votes)*100))) + "%" + " (" + str(candidate_votes[c]) + ")") 
+        candidate_results = (c + " " + str(round(((candidate_votes[c]/votes)*100))) + "%" + " (" + str(candidate_votes[c]) + ")")
+
+print("Winner:Khan")
